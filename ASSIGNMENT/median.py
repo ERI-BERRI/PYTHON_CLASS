138812bada84daa7f9d11654a8e31cad06271dec
Adebayo Awosanya <adebayoawosanya@Adebayos-iMac.local>
2023-06-21 17:03:43 +0300

    first commit

---
+++ ASSIGNMENT/median.py
@@ -0,0 +1,12 @@
+l = int(input("Enter your first number :"))
+
+b = int(input("Enter your second number :"))
+
+if l = b :
+
+print("Its a square")
+
+else :
+
+print("its an rectangle")
+
---
+++ BETA.py
@@ -0,0 +1,14 @@
+
+print( "Welcome to your digital calculator" )
+input( "Select an opperation" )
+
+s
+
+x= input( "Type a number" )
+y= input( "Type another number" )
+
+sum= int( x ) + int( y )
+
+print( "The numbers added together are", sum ) 
+
+input("select another operation")
